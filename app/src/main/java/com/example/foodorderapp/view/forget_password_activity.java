39ee525073d0ea620d35e424eb6d1ae18c394a7a
Khoa Dang <85307735+khoa1412001@users.noreply.github.com>
2022-05-10 03:19:26 +0700

    chuc nang gui mail quen mat khau

--- app/src/main/java/com/example/foodorderapp/view/forget_password_activity.java
+++ app/src/main/java/com/example/foodorderapp/view/forget_password_activity.java
@@ -1,16 +1,54 @@
 package com.example.foodorderapp.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.foodorderapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class forget_password_activity extends AppCompatActivity {
-
+    EditText txtEmail;
+    Button btnSendMail;
+    TextView txtlinkDK;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+        firebaseAuth = FirebaseAuth.getInstance();
+        txtEmail = findViewById(R.id.txtGuiMailResetMK);
+        btnSendMail = findViewById(R.id.btnGui);
+        btnSendMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = txtEmail.getText().toString();
+                firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful())
+                            Toast.makeText(forget_password_activity.this, "Gửi mail thành công", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(forget_password_activity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        txtlinkDK = findViewById(R.id.txtDangKyTaiDay);
+        txtlinkDK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(forget_password_activity.this,register_activity.class));
+            }
+        });
     }
 }
